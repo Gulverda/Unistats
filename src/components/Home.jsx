82a77648de18423b7dd56f1951056bc122ee8471
Duda Elbakidze <dudaelbakidze94@gmail.com>
2024-04-15 01:20:32 +0400

    making annualStats

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,45 +1,13 @@
 import React from "react";
-import logo from'../assets/fullLogo.png'
-
+import AnnualStats from "./annualStats.jsx"; 
 
 export default function Home() {
   return (
-    <nav className="flex flex-col items-left px-9 py-6">
-      <div className="my-4">
-        <img
-          src={logo}
-          alt="Logo"
-          className="h-12 w-auto"
-        />
+    <div className="w-full h-screen bg-customGray">
+      <div>
+        <AnnualStats />
       </div>
-
-      <ul className="flex flex-col items-left">
-        <li className="py-2">
-          <a href="#" className="text-gray-800 hover:text-blue-500">
-            Home
-          </a>
-        </li>
-        <li className="py-2">
-          <a href="#" className="text-gray-800 hover:text-blue-500">
-            About
-          </a>
-        </li>
-        <li className="py-2">
-          <a href="#" className="text-gray-800 hover:text-blue-500">
-            Services
-          </a>
-        </li>
-        <li className="py-2">
-          <a href="#" className="text-gray-800 hover:text-blue-500">
-            Products
-          </a>
-        </li>
-        <li className="py-2">
-          <a href="#" className="text-gray-800 hover:text-blue-500">
-            Contact
-          </a>
-        </li>
-      </ul>
-    </nav>
+    </div>
   );
 }
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customGray: '#F5F5F5',
+        customPink: '#FFE2E5',
+        customOrange: '#FFF4DE',
+        customGreen: '#DCFCE7',
+        customPurple: '#F3E8FF',
+      },
+      fontFamily: {
+        tkt: ['tkt'],
+        tktMedium: ['tktMedium'],
+      },
+    },
   },
   plugins: [],
 }
-

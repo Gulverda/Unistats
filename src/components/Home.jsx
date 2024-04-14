670be7fcd4d194d79197fa7d33df4e4c3937600a
Duda Elbakidze <dudaelbakidze94@gmail.com>
2024-04-15 01:44:15 +0400

    annualStats

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,11 +3,10 @@ import AnnualStats from "./annualStats.jsx";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen bg-customGray">
-      <div>
+    <div className="w-full h-screen bg-customGray flex">
+      <div className="max-w-screen-lg mx-auto py-16">
         <AnnualStats />
       </div>
     </div>
   );
 }
-

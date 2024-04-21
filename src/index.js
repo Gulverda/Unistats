c05e7c72234cb0f7293038c96e75b71fe1ff262f
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2024-04-21 22:59:15 +0400

    Updated React-18 and fix some error in console

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
+import { createRoot } from 'react-dom/client'; // Import createRoot from "react-dom/client"
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
+const root = document.getElementById('root');
+createRoot(root).render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
 );

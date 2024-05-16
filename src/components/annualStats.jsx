b3fe337b7c160c994d265cdd8509d2169323e720
Nika <nikadvalishvili517@gmail.com>
2024-05-16 21:27:38 +0400

    added countUp

--- src/components/annualStats.jsx
+++ src/components/annualStats.jsx
@@ -3,6 +3,7 @@ import icon from "../assets/Icon.png";
 import icon1 from "../assets/Icon (1).png";
 import icon2 from "../assets/Icon (2).png";
 import icon3 from "../assets/Icon (3).png";
+import CountUp from "react-countup";
 
 export default function AnnualStats() {
   return (
@@ -16,7 +17,10 @@ export default function AnnualStats() {
         <div className="child-for-grid w-36 h-34 px-4 py-3 bg-customPink rounded-xl flex flex-col gap-3">
           <img className="w-9 h-9 " src={icon} alt="" srcSet="" />
           <div className="flex flex-col gap-2">
-            <h2 className="font-PoppinsRegular font-semibold text-xl">250k+</h2>
+            <div className="font-PoppinsRegular font-semibold text-xl">
+              <CountUp start={0} end={250} duration={2}></CountUp>
+              K+
+            </div>
             <h4 className="font-tktMedium  text-xs text-gray-500">სტუდენტი</h4>
             <h4 className="font-tktMedium text-xs text-customBlue">
               +8% მეტი ვიდრე 2023 წელს
@@ -27,7 +31,11 @@ export default function AnnualStats() {
         <div className="child-for-grid w-36 h-34 px-4 py-3 bg-customOrange rounded-xl flex flex-col gap-3">
           <img className="w-9 h-9 " src={icon1} alt="" srcSet="" />
           <div className="flex flex-col gap-2">
-            <h2 className="font-PoppinsRegular font-semibold text-xl">42k+</h2>
+            <div className="font-PoppinsRegular font-semibold text-xl">
+              <CountUp start={0} end={42} duration={2}></CountUp>
+              K+
+            </div>
+
             <h4 className="font-tktMedium  text-xs text-gray-500">
               აბიტურიენტი
             </h4>
@@ -40,7 +48,14 @@ export default function AnnualStats() {
         <div className="child-for-grid w-36 h-34 px-4 py-3 bg-customGreen rounded-xl flex flex-col gap-3">
           <img className="w-9 h-9 " src={icon2} alt="" srcSet="" />
           <div className="flex flex-col gap-2">
-            <h2 className="font-PoppinsRegular font-semibold text-xl">61</h2>
+            <CountUp
+              start={0}
+              end={61}
+              duration={2}
+              className="font-PoppinsRegular font-semibold text-xl"
+            >
+              {/* <h2 className="font-PoppinsRegular font-semibold text-xl">61</h2> */}
+            </CountUp>
             <h4 className="font-tktMedium  text-xs text-gray-500">
               უნივერსიტეტი
             </h4>
@@ -53,7 +68,10 @@ export default function AnnualStats() {
         <div className="child-for-grid w-36 h-34 px-4 py-3 bg-customPurple rounded-xl flex flex-col gap-3">
           <img className="w-9 h-9 " src={icon3} alt="" srcSet="" />
           <div className="flex flex-col gap-0.5">
-            <h2 className="font-PoppinsRegular font-semibold text-xl">12k+</h2>
+            <div className="font-PoppinsRegular font-semibold text-xl">
+              <CountUp start={0} end={12} duration={2}></CountUp>
+              K+
+            </div>
             <h4 className="font-tktMedium  text-xs text-gray-500">
               საერთაშორისო სტუდენტი
             </h4>
